2faa265ce62fba7040a5a49ec36ec102cfdcefca
Vivek <vivekbumb@gmail.com>
2025-03-16 21:43:34 +0530

    Participant api created and integrated on client side

--- client/Screens/EventsScreen.js
+++ client/Screens/EventsScreen.js
@@ -15,8 +15,10 @@ import AppPrimaryButton from "../components/AppPrimaryButton";
 import { ACCOUNT_TYPE_ADMIN } from "../Screens/ProfilePage";
 import { useNavigation } from "@react-navigation/native";
 import axios from "axios";
-import MY_IP_ADDRESS from "../environment_variables.mjs";
-import { getUserToken, storeUserToken } from "../utils/StorageUtils";
+import {
+  addParticipantRequest,
+  getAllEvent,
+} from "../redux/actions/eventAction";
 
 const EventsScreen = () => {
   // State management
@@ -29,9 +31,9 @@ const EventsScreen = () => {
   // Get user info from Redux store to check if admin
   const userInfo = useSelector((state) => state.userInfo?.userInfo);
   const token = useSelector((state) => state.userInfo?.token);
+  const { _id } = userInfo;
 
   const isAdmin = userInfo?.user_roles == ACCOUNT_TYPE_ADMIN;
-
 
   // Fetch events when screen loads or returns to focus
   useEffect(() => {
@@ -41,18 +43,25 @@ const EventsScreen = () => {
   // Fetch events from API
   const fetchEvents = async () => {
     try {
-      const response = await axios.get(`http://${MY_IP_ADDRESS}:5050/events`, {
-        headers: {
-          'Authorization': `Bearer ${token}`
-        }
-      });
-      
-       setEvents(response.data.event);
-       setOldEvent(response.data.event);
+      const response = await getAllEvent({ token });
+      setEvents(response.data.event);
+      setOldEvent(response.data.event);
     } catch (error) {
-      console.error('Error in fetchEvents:', error);
+      console.error("Error in fetchEvents:", error);
       Alert.alert("Error", "Failed to load events");
       setEvents([]);
+    }
+  };
+
+  const addParticipant = async (eventId) => {
+    const response = await addParticipantRequest({
+      userId: _id,
+      token,
+      eventId,
+    });
+    if (response.status == 200) {
+      Alert.alert("Action !!", response.data);
+      fetchEvents();
     }
   };
 
@@ -67,79 +76,112 @@ const EventsScreen = () => {
 
   // Component to display individual event details
   const EventCard = ({ event }) => {
+    const isParticipated = event.participants
+      ? event.participants.includes(_id)
+      : false;
+
     return (
       <View style={styles.eventCard}>
         {/* Event Title */}
-        <Text style={styles.eventTitle}>
-          {event.event_title}
-        </Text>
-        
+        <Text style={styles.eventTitle}>{event.event_title}</Text>
+
         {/* Event Location */}
         <View style={styles.locationContainer}>
-          <Text style={styles.eventLocation}>
-            {event.event_location}
-          </Text>
+          <Text style={styles.eventLocation}>{event.event_location}</Text>
         </View>
-        
+
         {/* Event Description */}
         <Text style={styles.eventDescription} numberOfLines={3}>
           {event.event_desc}
         </Text>
-        
+
         {/* Event Link */}
-        <TouchableOpacity 
-          onPress={() => Linking.openURL(event.event_link)}
-          style={styles.linkContainer}
+        <View
+          style={{
+            flexDirection: "row",
+            borderTopWidth: 1,
+            borderTopColor: "#e0e0e0",
+            flex: 1,
+            justifyContent: "space-between",
+            paddingTop: 10,
+          }}
         >
-          <Text style={styles.eventLink}>View Event</Text>
-        </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => Linking.openURL(event.event_link)}
+            style={styles.linkContainer}
+          >
+            <Text style={styles.eventLink}>View Event</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            disabled={isParticipated}
+            onPress={() => {
+              isAdmin ? navigation.navigate("ParticipantList", { event }) :addParticipant(event._id);
+            }}
+            style={[
+              styles.linkContainer,
+              { backgroundColor: isParticipated ? "#e0e0e0" : "#fff" },
+            ]} // Use the appropriate background color based on isParticipated styles.linkContainer}
+          >
+            <Text
+              style={[
+                styles.eventLink,
+                { color: isParticipated ? "#000" : "#0066cc" },
+              ]}
+            >
+              {" "}
+              {isAdmin
+                ? "Participant list"
+                : isParticipated
+                ? "Requested"
+                : "Add Participate"}
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   };
 
-  const _getEventDate = ()=>{
+  const _getEventDate = () => {
     let eventDate = {};
-    oldEvent.map(event => {
+    oldEvent.map((event) => {
       eventDate[event.event_date] = {
         marked: true,
-        dotColor: "#097969"
+        dotColor: "#097969",
       };
     });
-   if(selectedDate){
-    eventDate[selectedDate] = {
-      selected: true,
-      selectedColor: "#097969"
+    if (selectedDate) {
+      eventDate[selectedDate] = {
+        selected: true,
+        selectedColor: "#097969",
+      };
     }
-   } 
-   return eventDate;
-  }
+    return eventDate;
+  };
 
-  const _filterEvent =(day)=>{
+  const _filterEvent = (day) => {
     setSelectedDate(day.dateString);
 
-    const result = oldEvent.filter(event => {
+    const result = oldEvent.filter((event) => {
       return event.event_date == day.dateString;
     });
-    setEvents(result)
-  }
+    setEvents(result);
+  };
 
-  
   return (
-    <ScrollView style={{ backgroundColor: 'white' }}>
+    <ScrollView style={{ backgroundColor: "white" }}>
       <View style={styles.container}>
         {/* Calendar Component */}
         <View style={styles.calendarContainer}>
           <Calendar
             style={{
-              width: Dimensions.get('screen').width,
-              alignSelf: 'center',
+              width: Dimensions.get("screen").width,
+              alignSelf: "center",
             }}
             onDayPress={(day) => {
-              _filterEvent(day)
+              _filterEvent(day);
             }}
-            markedDates={
-              _getEventDate()
-            }
+            markedDates={_getEventDate()}
             theme={{
               textDayFontSize: 16,
               todayTextColor: "#097969", // Set today's date text color to green
@@ -151,7 +193,7 @@ const EventsScreen = () => {
         {/* Add Event Button (Admin Only) */}
         {isAdmin && (
           <View style={styles.addEventButtonContainer}>
-            <View style={{ width: '90%' }}>
+            <View style={{ width: "90%" }}>
               <AppPrimaryButton
                 title={"Add Event"}
                 handleSubmit={() => {
@@ -163,13 +205,13 @@ const EventsScreen = () => {
         )}
 
         <View style={styles.eventsListContainer}>
-         { 
-        events.length == 0?
-          <Text> No event for selected date</Text> :
-         events.map((event, index) => (
-                <EventCard key={index} event={event} />
-              ))
-          }
+          {events.length == 0 ? (
+            <Text> No event for selected date</Text>
+          ) : (
+            events.map((event, index) => (
+              <EventCard key={index} event={event} />
+            ))
+          )}
         </View>
       </View>
     </ScrollView>
@@ -179,30 +221,30 @@ const EventsScreen = () => {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "white",
-    paddingBottom: 20
+    paddingBottom: 20,
   },
   calendarContainer: {
-    width: '100%',
-    backgroundColor: 'white'
+    width: "100%",
+    backgroundColor: "white",
   },
   addEventButtonContainer: {
     width: "100%",
     alignItems: "center",
     marginVertical: 10,
-    backgroundColor: 'white'
+    backgroundColor: "white",
   },
   eventsListContainer: {
-    alignItems: 'center',
+    alignItems: "center",
     paddingVertical: 10,
-    backgroundColor: 'white'
+    backgroundColor: "white",
   },
   eventCard: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderRadius: 10,
     padding: 15,
     marginVertical: 8,
     marginHorizontal: 16,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 2,
@@ -210,54 +252,49 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
     elevation: 5,
-    width: '90%',
+    width: "90%",
     borderWidth: 1,
-    borderColor: '#e0e0e0',
+    borderColor: "#e0e0e0",
   },
   eventTitle: {
     fontSize: 14,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 8,
-    color: '#333',
+    color: "#333",
   },
   locationContainer: {
     marginBottom: 8,
   },
   eventLocation: {
     fontSize: 14,
-    color: '#666',
+    color: "#666",
   },
   eventDescription: {
     fontSize: 14,
-    color: '#444',
+    color: "#444",
     marginBottom: 12,
     lineHeight: 20,
   },
-  linkContainer: {
-    marginTop: 8,
-    paddingTop: 8,
-    borderTopWidth: 1,
-    borderTopColor: '#e0e0e0',
-  },
+  linkContainer: {},
   eventLink: {
-    color: '#0066cc',
+    color: "#0066cc",
     fontSize: 14,
-    textDecorationLine: 'underline',
+    padding: 2,
   },
   noEventsText: {
     fontSize: 14,
-    color: '#666',
-    fontStyle: 'italic',
-    textAlign: 'center',
-    padding: 20
+    color: "#666",
+    fontStyle: "italic",
+    textAlign: "center",
+    padding: 20,
   },
   selectDateText: {
     fontSize: 14,
-    color: '#666',
-    fontStyle: 'italic',
-    textAlign: 'center',
-    padding: 20
-  }
+    color: "#666",
+    fontStyle: "italic",
+    textAlign: "center",
+    padding: 20,
+  },
 });
 
 export default EventsScreen;
