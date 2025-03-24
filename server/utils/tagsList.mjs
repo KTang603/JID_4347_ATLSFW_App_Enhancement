fa10655ebbb14873e66f518437d704544f23af08
Vivek <vivekbumb@gmail.com>
2025-03-24 21:19:05 +0530

    Latest filter added on news list

--- server/utils/tagsList.mjs
+++ server/utils/tagsList.mjs
@@ -1,4 +1,4 @@
-const tagsList = ["innovation", "environment", "brand-release", "fashion-week", "life-hacks", 
+const tagsList = ["Latest","innovation", "environment", "brand-release", "fashion-week", "life-hacks", 
     "eco-friendly", "upcycled-fashion", "thrifting", "vegan", "organic", "food-sustainability",
     "workforce", "luxury-brand", "business-stories"];
 export default tagsList;
